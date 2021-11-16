a06634ae0eff76b4a3ef4200901fbba253a79760
harshadshaik8860 <harshadshaik8860@gmail.com>
2021-11-16 17:31:10 +0530

    new code

--- accpro/src/Pages/Home.js
+++ accpro/src/Pages/Home.js
@@ -1,35 +1,81 @@
 import React from 'react';
+import Dashboard from '../components/Dashboard';
+import Header from './Header';
 import { Link } from 'react-router-dom';
-import {Button} from 'react-bootstrap'
 
-
-const Home = () => {
+const  Home = (props)=> {
     return (
-    <div>
-    <header className='header'>
-      <div >
-        <Link  className='links' to='/'>
-          BMS
-        </Link>
-      </div>
+        <div>
+            <Header />
+            {/* <div className="row mt-2">
+                <div className="col-lg-3">
+                    <ul className="list-group text-center">
+                        <li className="list-group-item color">Dashboards</li>
+                        <li className="list-group-item"> 
+                        <Link to="/dashboard"> Portfolio Dashboards</Link>
+                        </li>
+                        <li className="list-group-item"> Study Dashboards</li>
 
-      <nav className='navbar'>
-        <ul>
-           <Link  className="links" to='/dashboards'>
-            Dashboards
-          </Link>
-         <button className="button"> <Link className="link" to='/logout'>
-            Logout
-          </Link></button>
-        </ul>
-      </nav>
-    </header>
+                    </ul>
+                </div>
+                </div> */}
 
+                <div className="container">
+                    <h1 className="mt-5"> DASHBOARDS</h1>
+                    <div className="row mt-5">
+                        <div className="col-lg-4">
+                            <div className="card text-center p-5">
+                                <div className="card-body">
+                                    Portfolio DashBoard
+                                </div>
+                            </div>
+                        </div>
 
-     <h1>Welcom Home</h1>
-  </div>
-        
+                        <div className="col-lg-4">
+                            <div className="card text-center p-5">
+                                <div className="card-body">
+                                    Study DashBoard
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4">
+                            <div className="card text-center p-5">
+                                <div className="card-body">
+                                    Functional DashBoard
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-lg-4">
+                            <div className="card text-center p-5">
+                                <div className="card-body">
+                                    Portfolio DashBoard
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4">
+                            <div className="card text-center p-5">
+                                <div className="card-body">
+                                    Study DashBoard
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4">
+                            <div className="card text-center p-5">
+                                <div className="card-body">
+                                    Functional DashBoard
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                </div>
+        </div>
     );
-};
+}
 
 export default Home;
